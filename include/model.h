b93c52b2fd58e5655c8ced1e898e8a1218602ddc
Rune Frohn <rfrohn@t-online.de>
2020-07-31 17:37:45 +0200

    add Makefile and model

---
+++ include/model.h
@@ -0,0 +1,26 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "layer.h"
+#include "tensor.h"
+
+typedef struct model_layer{
+	struct model_layer* next_layer;
+	struct model_layer* prev_layer;
+	layer_t* layer;
+	int is_last;	
+} model_layer_t;
+
+typedef struct model {
+	model_layer_t* first_layer;
+} model_t;
+
+model_t* create_model();
+void free_model(model_t** model);
+void add_layer(model_t* model, layer_t* layer);
+model_layer_t* get_last_layer(model_t* model);
+tensor_handle_t* predict(model_t* model, tensor_handle_t* input);
+void train(model_t* model, tensor_handle_t* input, tensor_handle_t* y, int epochs, float learning_rate);
+void print_model(model_t* model);
+
+#endif /* MODEL_H */
---
+++ src/model.c
@@ -0,0 +1,121 @@
+// keras style model functionality
+
+#include "model.h"
+#include "layer.h"
+#include "tensor.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+model_t* create_model(){
+	model_t* model = malloc(sizeof(model_t));
+	//TODO: check if model is not NULL
+	if(model == NULL){
+		printf("ERROR: couldn't alloc memory for the model\n");
+	}
+	model->first_layer = NULL;
+	return model;
+}
+
+void free_model(model_t** model){
+	if(*model == NULL){
+		printf("WARNING: trying to free a model that is allready freed!\n");
+	}	
+	// free all other layers.
+
+	model_layer_t* c_layer = (*model)->first_layer;
+	while(c_layer != NULL && !c_layer->is_last){
+		model_layer_t* layer_tmp = c_layer;
+		c_layer = c_layer->next_layer;
+		free_layer(&layer_tmp->layer);
+		free(layer_tmp);
+	}
+}
+
+void add_layer(model_t* model, layer_t* layer){
+	// check if the added layer is the first one. Then we create a new link list...
+	model_layer_t* new_model_layer = malloc(sizeof(model_layer_t));
+	if(new_model_layer){
+		printf("ERROR: failed to alloc and therefore to add layer to model\n");
+	}
+	new_model_layer->is_last = 1;
+	new_model_layer->layer = layer;
+	if(model->first_layer == NULL){
+		new_model_layer->next_layer = new_model_layer;
+		new_model_layer->prev_layer = new_model_layer;
+	} else {
+		model_layer_t* last = get_last_layer(model);
+		//Fix pointer
+		last->is_last = 0;
+		// connection to prev last layer
+		last->next_layer = new_model_layer;
+		new_model_layer->prev_layer = last;
+		// connection to first layer
+		new_model_layer->next_layer = model->first_layer;
+		model->first_layer->prev_layer = new_model_layer;
+	}
+}
+
+model_layer_t* get_last_layer(model_t* model){
+	if(model->first_layer == NULL){
+		return NULL;
+	} else {
+		return model->first_layer->prev_layer;
+	}
+}
+
+tensor_handle_t* predict(model_t* model, tensor_handle_t* input){
+	tensor_handle_t* next_input = input;
+	model_layer_t* c_layer = model->first_layer;
+	while(c_layer != NULL && !c_layer->is_last){
+		tensor_handle_t* tmp = forward_pass(c_layer->layer, next_input);
+		// we free tensor that are in the middle
+		if(input != next_input){
+			free_tensor(&next_input);
+		}
+		// go to next layer
+		c_layer = c_layer->next_layer;
+	}
+	return next_input;
+}
+
+void train(model_t* model, tensor_handle_t* input, tensor_handle_t* y, int epochs, float learning_rate){
+	for(int epoch=0; epoch < epochs; epoch++){
+		tensor_handle_t* model_output = predict(model, input);
+		
+		// print error
+		tensor_handle_t* error = squared_loss(model_output, y);
+		float sum = tensor_sum(error);
+		int size = get_tensor_size(error);
+		printf("epoch %d loss: %f", epoch, sum/size);
+		
+		// TODO: support other error functions
+		tensor_handle_t* next_error = squared_loss_derivative(model_output, y);	
+		free_tensor(&model_output);
+			
+		// go backwards thru all the layers
+		model_layer_t* c_layer = get_last_layer(model);
+		while(c_layer != NULL && c_layer != model->first_layer){
+			tensor_handle_t* tmp = backward_pass(c_layer->layer, next_error, learning_rate);
+			// we free the tensor that are in the middle
+			free_tensor(&next_error);
+			next_error = tmp;
+			// go to prev layer
+			c_layer = c_layer->prev_layer;
+		}
+		free_tensor(&next_error);
+	}
+}
+
+void print_model(model_t* model){
+	printf("Model at %p\n", model);
+	int layer_index = 0;
+	
+	model_layer_t* c_layer = model->first_layer;
+	while(c_layer != NULL && !c_layer->is_last){
+		int* shape = c_layer->layer->weights->shape;
+		printf("Layer %d shape: [%d, %d]", layer_index, shape[0], shape[1]);
+		// go to next layer
+		c_layer = c_layer->next_layer;
+		layer_index++;
+	}
+}
